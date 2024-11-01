1654751fb282ac0a3dc53470c78e3fbbd41d833a
Gustavo Henrique Carl Dos Santos <gustavo_carl@live.com>
2024-11-01 10:19:42 -0300

    Added initial files

---
+++ src/Models/Base.cs
@@ -0,0 +1,12 @@
+﻿namespace Bar_Management_System.Models
+{
+    public abstract class Base
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Base(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Bar_Management_System.Models
+{
+    public class User : Base
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public User(Guid id, string firstName, string lastName) : base(id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
